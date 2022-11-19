f3f1e5ee55f1daf8adee888bfbc13d86aa00daca
Philipp-hfg <64197165+Philipp-hfg@users.noreply.github.com>
2022-11-19 16:57:47 +0100

    Update Donnerstag

---
+++ L06/task_material/main.js
@@ -0,0 +1,16 @@
+let DE_Einwohner = 83.1;
+let FR_Einwohner = 65.3;
+let IT_Einwohner = 59;
+let SP_Einwohner = 47.4;
+
+let Jahr = 2021;
+
+const DE = "Deutschland";
+const FR = "Frankreich";
+const IT = "Italien";
+const SP = "Spanien";
+
+console.log("Die Einwoherzahl in " + DE + " beträgt " + DE_Einwohner + " im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + FR + " beträgt " + FR_Einwohner + " im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + IT + " beträgt " + IT_Einwohner + " im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + SP + " beträgt " + SP_Einwohner + " im Jahr " + Jahr + ".");

53966bc228ece447222188671b2d407ed4b28ec6
Philipp-hfg <philipp.kicherer@hfg.design>
2022-11-19 17:19:11 +0100

    Update Samstag

--- L06/task_material/main.js
+++ L06/task_material/main.js
@@ -1,16 +1,14 @@
-let DE_Einwohner = 83.1;
-let FR_Einwohner = 65.3;
-let IT_Einwohner = 59;
+let DE_Einwohner = 83.2;
+let FR_Einwohner = 67.8;
+let IT_Einwohner = 59.0;
 let SP_Einwohner = 47.4;
-
-let Jahr = 2021;
-
+let Jahr = 2022;
 const DE = "Deutschland";
 const FR = "Frankreich";
 const IT = "Italien";
 const SP = "Spanien";
-
-console.log("Die Einwoherzahl in " + DE + " beträgt " + DE_Einwohner + " im Jahr " + Jahr + ".");
-console.log("Die Einwoherzahl in " + FR + " beträgt " + FR_Einwohner + " im Jahr " + Jahr + ".");
-console.log("Die Einwoherzahl in " + IT + " beträgt " + IT_Einwohner + " im Jahr " + Jahr + ".");
-console.log("Die Einwoherzahl in " + SP + " beträgt " + SP_Einwohner + " im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + DE + " beträgt " + DE_Einwohner + " Millionen im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + FR + " beträgt " + FR_Einwohner + " Millionen im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + IT + " beträgt " + IT_Einwohner + " Millionen im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + SP + " beträgt " + SP_Einwohner + " Millionen im Jahr " + Jahr + ".");
+//# sourceMappingURL=main.js.map
---
+++ L06/task_material/main.ts
@@ -0,0 +1,16 @@
+let DE_Einwohner = 83.2;
+let FR_Einwohner = 67.8;
+let IT_Einwohner = 59.0;
+let SP_Einwohner = 47.4;
+
+let Jahr = 2022;
+
+const DE = "Deutschland";
+const FR = "Frankreich";
+const IT = "Italien";
+const SP = "Spanien";
+
+console.log("Die Einwoherzahl in " + DE + " beträgt " + DE_Einwohner + " Millionen im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + FR + " beträgt " + FR_Einwohner + " Millionen im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + IT + " beträgt " + IT_Einwohner + " Millionen im Jahr " + Jahr + ".");
+console.log("Die Einwoherzahl in " + SP + " beträgt " + SP_Einwohner + " Millionen im Jahr " + Jahr + ".");

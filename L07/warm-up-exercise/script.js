d3ac6caf7d90791d3811ec0fa4d20630b7d948d2
Philipp-hfg <64197165+Philipp-hfg@users.noreply.github.com>
2022-11-23 22:48:11 +0100

    Update mit ts

--- L07/warm-up-exercise/script.js
+++ L07/warm-up-exercise/script.js
@@ -1,18 +1,16 @@
 //Aufgabe 1
 function finger() {
     document.querySelector("#button1").innerHTML = "Der dickste Finger an der Hand ist der Daumen";
-};
-
+}
+;
 document.querySelector("#button1").addEventListener('click', finger);
-
-
 //Aufgabe 2
 let count = 0;
-
 function button_click() {
     let new_count = (count + 1);
     count = new_count;
     document.querySelector("#result").innerHTML = count.toString(); //hier nach der Funktion .toString(), weil typescript das so will (in js gehts auch ohne das).
-};
-
+}
+;
 document.querySelector("#button2").addEventListener('click', button_click);
+//# sourceMappingURL=script.js.map

54e6d3016753fd9ba37c463e298f29a286f32d50
Philipp-hfg <64197165+Philipp-hfg@users.noreply.github.com>
2022-11-23 22:34:26 +0100

    Update?

--- L07/warm-up-exercise/script.ts
+++ L07/warm-up-exercise/script.ts
@@ -1,21 +1,18 @@
+//Aufgabe 1
 function finger() {
-    console.log("Der dickste Finger an der Hand ist der Daumen")
     document.querySelector("#button1").innerHTML = "Der dickste Finger an der Hand ist der Daumen";
 };
 
 document.querySelector("#button1").addEventListener('click', finger);
 
 
+//Aufgabe 2
 let count = 0;
 
 function button_click() {
     let new_count = (count + 1);
     count = new_count;
-};
-
-function anzeige() {
     document.querySelector("#result").innerHTML = count;
 };
 
 document.querySelector("#button2").addEventListener('click', button_click);
-document.querySelector("#button2").addEventListener('click', anzeige);

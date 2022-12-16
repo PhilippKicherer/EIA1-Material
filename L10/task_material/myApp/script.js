b5bbff48eefa1e99ad70ea6b71a5d14e4387e525
Philipp-hfg <philipp.kicherer@hfg.design>
2022-12-16 22:42:15 +0100

    Update Friday

--- L10/task_material/myApp/script.js
+++ L10/task_material/myApp/script.js
@@ -1,11 +1,41 @@
 //Variablen
+let enter = document.getElementById("inputText");
 //Funktionen
 //Funktion, um eine Task hinzuzufügen
+function addTask() {
+    let div = document.createElement("div");
+    div.className = "task-item";
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox";
+    let label = document.createElement("label");
+    label.innerHTML = enter.value;
+    label.className = "taskText";
+    let trash = document.createElement("i");
+    trash.className = "far fa-trash-alt";
+    document.getElementById("ToDoList").appendChild(div);
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    div.appendChild(trash);
+    function deleteTask(div) {
+        div.remove();
+    }
+    trash.addEventListener("click", function (event) {
+        console.log(event);
+        deleteTask(div);
+    });
+}
 //Funktion, um eine Task als "done" zu markieren
 //Funktion, um eine Task zu löschen
 //(passive) Funktion, um die Anzahl der Tasks wiederzugeben
 //Events
 //Key-Event, um eine Task hinzuzufügen
+enter.addEventListener("keypress", function (event) {
+    if (event.key == "Enter") {
+        addTask();
+        enter.value = "";
+    }
+});
 //Click-Event, um eine Task als "done" zu markieren (for-Schleife)
 //Click-Event, um eine Task zu löschen (for-Schleife)
 //# sourceMappingURL=script.js.map
--- L10/task_material/myApp/script.ts
+++ L10/task_material/myApp/script.ts
@@ -1,18 +1,61 @@
 //Variablen
+let enter: HTMLInputElement = (document.getElementById("inputText") as HTMLInputElement);
 
 //Interface (freiwillig, ansonsten wird nächste Woche hinzugefügt)
 interface Task {
-    Name: string;
-    state: Boolean;
+    id: number;
+    name: string;
+    done: Boolean;
 }
 
 //Funktionen
 //Funktion, um eine Task hinzuzufügen
+function addTask() {
+
+    let div: HTMLDivElement = document.createElement("div");
+    div.className = "task-item";
+
+    let checkbox: HTMLInputElement = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox";
+
+    let label: HTMLLabelElement = document.createElement("label");
+    label.innerHTML = enter.value;
+    label.className = "taskText";
+
+    let trash: HTMLElement = document.createElement("i");
+    trash.className = "far fa-trash-alt";
+
+    document.getElementById("ToDoList").appendChild(div);
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    div.appendChild(trash);
+
+    function deleteTask(div: HTMLElement) {
+        div.remove();
+    }
+
+    trash.addEventListener("click", function (event: MouseEvent) {
+        console.log(event);
+        deleteTask(div);
+    });
+}
+
 //Funktion, um eine Task als "done" zu markieren
+
 //Funktion, um eine Task zu löschen
+
 //(passive) Funktion, um die Anzahl der Tasks wiederzugeben
 
 //Events
 //Key-Event, um eine Task hinzuzufügen
+
+enter.addEventListener("keypress", function (event: KeyboardEvent): void {
+    if (event.key == "Enter") {
+        addTask();
+        enter.value = "";
+    }
+});
+
 //Click-Event, um eine Task als "done" zu markieren (for-Schleife)
 //Click-Event, um eine Task zu löschen (for-Schleife)

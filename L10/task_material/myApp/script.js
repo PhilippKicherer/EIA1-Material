c2ca2eec754c5426eb9fbba67d569ee9b0c217bb
Philipp-hfg <64197165+Philipp-hfg@users.noreply.github.com>
2022-12-16 21:09:06 +0100

    Update Thursday

---
+++ L10/task_material/myApp/script.js
@@ -0,0 +1,11 @@
+//Variablen
+//Funktionen
+//Funktion, um eine Task hinzuzufügen
+//Funktion, um eine Task als "done" zu markieren
+//Funktion, um eine Task zu löschen
+//(passive) Funktion, um die Anzahl der Tasks wiederzugeben
+//Events
+//Key-Event, um eine Task hinzuzufügen
+//Click-Event, um eine Task als "done" zu markieren (for-Schleife)
+//Click-Event, um eine Task zu löschen (for-Schleife)
+//# sourceMappingURL=script.js.map
---
+++ L10/task_material/myApp/script.ts
@@ -0,0 +1,18 @@
+//Variablen
+
+//Interface (freiwillig, ansonsten wird nächste Woche hinzugefügt)
+interface Task {
+    Name: string;
+    state: Boolean;
+}
+
+//Funktionen
+//Funktion, um eine Task hinzuzufügen
+//Funktion, um eine Task als "done" zu markieren
+//Funktion, um eine Task zu löschen
+//(passive) Funktion, um die Anzahl der Tasks wiederzugeben
+
+//Events
+//Key-Event, um eine Task hinzuzufügen
+//Click-Event, um eine Task als "done" zu markieren (for-Schleife)
+//Click-Event, um eine Task zu löschen (for-Schleife)
